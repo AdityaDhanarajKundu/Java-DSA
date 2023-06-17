1454ed7f7d5370a2d68a4c286a4aee57a1d3b681
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-06-17 19:41:05 +0530

    Deletion Operation on Binary Search Tree

--- BinaryTree/BST.java
+++ BinaryTree/BST.java
@@ -47,6 +47,65 @@ class BinarySoln{
             inorder(root.right);
          }
     }
+
+    //deletion operation
+    Node deleteNode(Node root, int key) {
+        // Base case
+        if (root == null)
+            return root;
+ 
+        // Recursive calls for ancestors of
+        // node to be deleted
+        if (root.key > key) {
+            root.left = deleteNode(root.left, key);
+            return root;
+        } else if (root.key < key) {
+            root.right = deleteNode(root.right, key);
+            return root;
+        }
+ 
+        // We reach here when root is the node
+        // to be deleted.
+ 
+        // If one of the children is empty
+        if (root.left == null) {
+            Node temp = root.right;
+            return temp;
+        } else if (root.right == null) {
+            Node temp = root.left;
+            return temp;
+        }
+ 
+        // If both children exist
+        else {
+ 
+            Node succParent = root;
+ 
+            // Find successor
+            Node succ = root.right;
+            while (succ.left != null) {
+                succParent = succ;
+                succ = succ.left;
+            }
+ 
+            // Delete successor.  Since successor
+            // is always left child of its parent
+            // we can safely make successor's right
+            // right child as left of its parent.
+            // If there is no succ, then assign
+            // succ.right to succParent.right
+            if (succParent != root)
+                succParent.left = succ.right;
+            else
+                succParent.right = succ.right;
+ 
+            // Copy Successor Data to root
+            root.key = succ.key;
+ 
+            // Delete Successor and return root
+            return root;
+        }
+    }
 }
 public class BST {
     public static void main(String[] args) {
@@ -60,5 +119,8 @@ public class BST {
         t.insertdata(55);
 
         t.inorder(t.root);
+        t.deleteNode(t.root, 35);
+        System.out.println();
+        t.preorder(t.root);
     }
 }
