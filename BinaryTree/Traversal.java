3ebcb3faf28753d53f8518d2688bc05001fb8d77
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2023-06-17 13:30:55 +0530

    Binary Tree implementation

--- BinaryTree/Traversal.java
+++ BinaryTree/Traversal.java
@@ -9,12 +9,28 @@ class Node{
 
 class TreeTraversal{
     Node root;
+    //defining the traversal methods
     void preorder(Node n){
         if (n!=null){// it will run till it reaches the leaf node
             System.out.print(n.key+" ");
             preorder(n.left);
             preorder(n.right);
         }
+    }
+
+    void postorder(Node n){
+        if(n!=null){
+            postorder(n.left);
+            postorder(n.right);
+            System.out.print(n.key+" ");
+        }
+    }
+    void inorder(Node n){
+         if(n!=null){//the function will finish when it will the end of a subtree
+            inorder(n.left);
+            System.out.print(n.key+" ");
+            inorder(n.right);
+         }
     }
 }
 
@@ -30,5 +46,9 @@ public class Traversal{
 
         //calling the preorder traversal function
         t1.preorder(t1.root);
+        System.out.println();
+        t1.postorder(t1.root);
+        System.out.println();
+        t1.inorder(t1.root);
     }
 }
